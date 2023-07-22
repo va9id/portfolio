17ce7fcf0269bb04ef6ca88206550cb4b6df2319
vahido9 <97857304+vahido9@users.noreply.github.com>
2023-07-21 20:24:25 -0400

    removed listener

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-window.onload = function () {
-    document.getElementById("cards").onmousemove = e => {
-        for (const card of document.getElementsByClassName("card")) {
-            const rect = card.getBoundingClientRect(),
-                x = e.clientX - rect.left,
-                y = e.clientY - rect.top;
+document.getElementById("cards").onmousemove = e => {
+    for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
 
-            card.style.setProperty("--mouse-x", `${x}px`);
-            card.style.setProperty("--mouse-y", `${y}px`);
-        };
-    }
-} 
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    };
+}
+
