9c73979f313f815d3b56a7092255fa8b2d3cfa82
vahido9 <vayeeded@gmail.com>
2025-01-20 20:26:49 -0500

    cycle art on img click

--- assets/index.js
+++ assets/index.js
@@ -3,19 +3,23 @@ const images = ["art1.jpg", "art2.jpg", "art3.jpg", "art4.jpg", "art5.jpg", "art
 const isV4 = document.getElementById("art");
 const isV1orV2 = document.getElementById("cards");
 
-if (isV4) {
-    document.addEventListener("DOMContentLoaded", function () {
-        let index = parseInt(localStorage.getItem("imageIndex"), 10);
+const cycleArt = () => {
+    let index = parseInt(localStorage.getItem("imageIndex"), 10);
 
-        if (isNaN(index)) {
-            index = -1;
-        }
-        index = (index + 1) % images.length;
-        localStorage.setItem("imageIndex", index);
+    if (isNaN(index)) {
+        index = -1;
+    }
+    index = (index + 1) % images.length;
+    localStorage.setItem("imageIndex", index);
 
-        document.getElementById("art").src = "assets/imgs/" + images[index];
-    });
+    document.getElementById("art").src = "assets/imgs/" + images[index];
 }
+
+if (isV4) {
+    document.addEventListener("DOMContentLoaded", cycleArt);
+    isV4.addEventListener("click", cycleArt);
+}
+
 // mouse effect for v1 & v2
 if (isV1orV2) {
     document.getElementById("cards").onmousemove = e => {
