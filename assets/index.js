aa0ac30a8383f60ab97270e94151bb1cf5a4d5ac
vahido9 <vayeeded@gmail.com>
2025-01-20 20:06:17 -0500

    artwork

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,31 @@
-document.getElementById("cards").onmousemove = e => {
-    for (const card of document.getElementsByClassName("card")) {
-        const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
+const images = ["art1.jpg", "art2.jpg", "art3.jpg", "art4.jpg", "art5.jpg", "art6.jpg", "art7.jpg", "art8.jpg", "art9.jpg"];
 
-        card.style.setProperty("--mouse-x", `${x}px`);
-        card.style.setProperty("--mouse-y", `${y}px`);
-    };
+const isV4 = document.getElementById("art");
+const isV1orV2 = document.getElementById("cards");
+
+if (isV4) {
+    document.addEventListener("DOMContentLoaded", function () {
+        let index = parseInt(localStorage.getItem("imageIndex"), 10);
+
+        if (isNaN(index)) {
+            index = -1;
+        }
+        index = (index + 1) % images.length;
+        localStorage.setItem("imageIndex", index);
+
+        document.getElementById("art").src = "assets/imgs/" + images[index];
+    });
 }
+// mouse effect for v1 & v2
+if (isV1orV2) {
+    document.getElementById("cards").onmousemove = e => {
+        for (const card of document.getElementsByClassName("card")) {
+            const rect = card.getBoundingClientRect(),
+                x = e.clientX - rect.left,
+                y = e.clientY - rect.top;
 
+            card.style.setProperty("--mouse-x", `${x}px`);
+            card.style.setProperty("--mouse-y", `${y}px`);
+        };
+    }
+}
